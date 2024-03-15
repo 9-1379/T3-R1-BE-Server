99776a8c556fa20b8987ca4503257ac82f897c49
yjinnote <yjinlee99@gmail.com>
2024-03-15 12:01:40 +0900

    깃 이그노어 파일, spring 생성

---
+++ bhr/src/main/java/com/nineties/bhr/BhrApplication.java
@@ -0,0 +1,13 @@
+package com.nineties.bhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhrApplication.class, args);
+	}
+
+}
