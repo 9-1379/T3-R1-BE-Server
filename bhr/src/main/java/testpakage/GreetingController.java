19407a6f84af74b149d0e9813fa157b259aef583
leedonghyuk <ldh0308@naver.com>
2024-03-22 10:14:29 +0900

    [Add]WebConfig 추가

---
+++ bhr/src/main/java/testpakage/GreetingController.java
@@ -0,0 +1,16 @@
+package testpakage;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+//    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    public String greeting() {
+        return "Hello from Spring Boot";
+    }
+}
