b7717d730ec25f007357aa3506045658a0551dc0
yjin_note <yjinlee99@naver.com>
2024-03-25 12:25:35 +0900

    [Add] 신규직원 로직 생성

--- bhr/src/main/java/com/nineties/bhr/join/dto/JoinDTO.java
+++ bhr/src/main/java/com/nineties/bhr/join/dto/JoinDTO.java
@@ -1,15 +1,11 @@
-package com.nineties.bhr.emp.dto;
+package com.nineties.bhr.join.dto;
 
-import com.nineties.bhr.dept.domain.Dept;
 import com.nineties.bhr.emp.domain.Address;
 import com.nineties.bhr.emp.domain.Gender;
 import com.nineties.bhr.emp.domain.Role;
 import com.nineties.bhr.emp.domain.Status;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 
 import java.util.Date;
 
@@ -17,15 +13,16 @@ import java.util.Date;
 @Getter
 public class JoinDTO {
 
-    private Long empNo;
     private String name;
     private Gender gender;
     private String birthday;
     private String phoneNumber;
+    private String email;
     private String position;
     private String jobId;
     private Date hireDate;
+    private Address address;
     private String username;
     private String password;
-    private Status status = Status.WORKING;
+    private String deptName;
 }
--- bhr/src/main/java/com/nineties/bhr/join/service/JoinService.java
+++ bhr/src/main/java/com/nineties/bhr/join/service/JoinService.java
@@ -1,11 +1,10 @@
-package com.nineties.bhr.emp.service;
+package com.nineties.bhr.join.service;
 
-import com.nineties.bhr.emp.domain.Employees;
-import com.nineties.bhr.emp.domain.Gender;
-import com.nineties.bhr.emp.domain.Role;
-import com.nineties.bhr.emp.domain.Status;
-import com.nineties.bhr.emp.dto.JoinDTO;
+import com.nineties.bhr.dept.domain.Dept;
+import com.nineties.bhr.dept.repository.DeptRepository;
+import com.nineties.bhr.emp.domain.*;
 import com.nineties.bhr.emp.repository.EmployeesRepository;
+import com.nineties.bhr.join.dto.JoinDTO;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,48 +15,61 @@ public class JoinService {
 
     private final EmployeesRepository employeesRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final DeptRepository deptRepository;
 
-    public JoinService(EmployeesRepository employeesRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public JoinService(EmployeesRepository employeesRepository, BCryptPasswordEncoder bCryptPasswordEncoder, DeptRepository deptRepository) {
 
         this.employeesRepository = employeesRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.deptRepository = deptRepository;
     }
 
     public void joinProcess(JoinDTO joinDTO) {
 
-        Long empNo = joinDTO.getEmpNo();
         String name = joinDTO.getName();
         Gender gender = joinDTO.getGender();
         String birthday = joinDTO.getBirthday();
         String phoneNumber = joinDTO.getPhoneNumber();
+        String email = joinDTO.getEmail();
         String position = joinDTO.getPosition();
         String jobId = joinDTO.getJobId();
         Date hireDate = joinDTO.getHireDate();
+        Address address = joinDTO.getAddress();
         String username = joinDTO.getUsername();
         String password = joinDTO.getPassword();
-        Status status = joinDTO.getStatus();
+        Dept dept = deptRepository.findByDeptName(joinDTO.getDeptName());
 
         Boolean isExist = employeesRepository.existsByUsername(username);
 
         if (isExist) {
-
             return;
         }
 
         Employees data = new Employees();
 
-        data.setEmpNo(empNo);
+        Long maxEmpNo = employeesRepository.findMaxEmpNo();
+        if (maxEmpNo == null) {
+            maxEmpNo = 1L; // empNo가 아직 없으면, 초기값으로 1 설정
+        } else {
+            maxEmpNo += 1; // 기존 최대값에서 1 증가
+        }
+
+        data.setEmpNo(maxEmpNo);
         data.setName(name);
         data.setGender(gender);
         data.setBirthday(birthday);
         data.setPhoneNumber(phoneNumber);
+        data.setEmail(email);
         data.setPosition(position);
         data.setJobId(jobId);
+        data.setDept(dept);
         data.setHireDate(hireDate);
+        data.setAddress(address);
         data.setUsername(username);
         data.setPassword(bCryptPasswordEncoder.encode(password));
         data.setAuthorization(Role.HRMANAGER);
-        data.setStatus(status);
+        data.setStatus(Status.WORKING);
+        data.setAuthorization(Role.EMPLOYEE);
 
         employeesRepository.save(data);
     }
