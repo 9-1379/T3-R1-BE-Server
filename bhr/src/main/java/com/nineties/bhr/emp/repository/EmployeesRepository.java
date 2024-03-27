00457d366d5d14f7f2392b2c8d61dea0a17c94e4
yjin_note <yjinlee99@naver.com>
2024-03-27 10:39:42 +0900

    [Add] 직원 리스트 시작

--- bhr/src/main/java/com/nineties/bhr/emp/repository/EmployeesRepository.java
+++ bhr/src/main/java/com/nineties/bhr/emp/repository/EmployeesRepository.java
@@ -1,10 +1,13 @@
 package com.nineties.bhr.emp.repository;
 
+import com.nineties.bhr.dept.domain.Dept;
 import com.nineties.bhr.emp.domain.Employees;
+import com.nineties.bhr.hrcard.dto.EmpListDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,4 +20,7 @@ public interface EmployeesRepository extends JpaRepository<Employees, String> {
 
     @Query("SELECT MAX(e.empNo) FROM Employees e")
     Long findMaxEmpNo();
+
+    @Query("SELECT e.empNo, e.name, e.dept.deptName, e.email from Employees e")
+    List<EmpListDTO> findAllByDept(Dept dept);
 }
---
+++ bhr/src/main/java/com/nineties/bhr/hrcard/dto/EmpListDTO.java
@@ -0,0 +1,13 @@
+package com.nineties.bhr.hrcard.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmpListDTO {
+    Long empNo;
+    String name;
+    String deptName;
+    String email;
+}
