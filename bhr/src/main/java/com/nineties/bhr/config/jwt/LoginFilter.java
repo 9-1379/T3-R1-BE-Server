a7ff22ccd011d52953bf3ff34e565ae3d5bc6389
hyunsungJoo <joo@LAPTOP-M7ISC7UR.>
2024-04-08 17:50:20 +0900

    [Modify] 출퇴근 수정

--- bhr/src/main/java/com/nineties/bhr/config/jwt/LoginFilter.java
+++ bhr/src/main/java/com/nineties/bhr/config/jwt/LoginFilter.java
@@ -55,9 +55,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         GrantedAuthority auth = iterator.next();
 
         String role = auth.getAuthority();
-
-        String token = jwtUtil.createJwt(username, role, 60 * 60 * 24 * 10 * 1000L);
-
+        String token = jwtUtil.createJwt(username, role, 60*60*10L* 1000);
         response.addHeader("Authorization", "Bearer " + token);
     }
 
