1099a4d0c08dac9bc9286e9334264a4e46a6035a
yjin_note <yjinlee99@naver.com>
2024-04-16 17:16:03 +0900

    [Modify] 출근 현황 총인원, 출석, 결석, 휴가로 변경

--- src/main/java/com/nineties/bhr/attendance/domain/AttendanceStatus.java
+++ src/main/java/com/nineties/bhr/attendance/domain/AttendanceStatus.java
@@ -1,5 +1,5 @@
 package com.nineties.bhr.attendance.domain;
 
 public enum AttendanceStatus {
-    PRESENT, LEAVE, LATE, ABSENT, ON_LEAVE // 출근, 지각, 결근, 휴가
+    PRESENT, LATE, ABSENT, ON_LEAVE // 출근, 지각, 결근, 휴가
 }
